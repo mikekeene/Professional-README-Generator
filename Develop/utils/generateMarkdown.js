227fab08d0752c5f9bb2540c6c3d9524170d5cc0
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-01-09 18:39:37 -0600

    license

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,46 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
+function generateMarkdown(data) {
+  //license
+  let licenseOption = `${data.license}`;
+  let licenseLink = '';
+  //if conditional to give the license the correct link
+  if (licenseOption === 'GNU AGPLv3') {
+    licenseOption = 'GNUAGPLv3';
+    licenseLink = 'https://choosealicense.com/licenses/agpl-3.0/';
+  };
+  if (licenseOption === 'GNU GPLv3') {
+    licenseOption = 'GNUGPLv3';
+    licenseLink = 'https://choosealicense.com/licenses/gpl-3.0/';
+  };
+  if (licenseOption === 'GNU LGPLv3') {
+    licenseOption = 'GNULGPLv3';
+    licenseLink = 'https://choosealicense.com/licenses/lgpl-3.0/';
+  };
+  if (licenseOption === 'Mozilla Public License 2.0') {
+    licenseOption = 'MozillaPublicLicense2.0';
+    licenseLink = 'https://choosealicense.com/licenses/mpl-2.0/';
+  };
+  if (licenseOption === 'Apache License 2.0') {
+    licenseOption = 'ApacheLicense2.0';
+    licenseLink = 'https://choosealicense.com/licenses/apache-2.0/';
+  };
+  if (licenseOption === 'MIT License') {
+    licenseOption = 'MITLicense';
+    licenseLink = 'https://choosealicense.com/licenses/mit/';
+  };
+  if (licenseOption === 'Boost Software License 1.0') {
+    licenseOption = 'BoostSoftwareLicense1.0';
+    licenseLink = 'https://choosealicense.com/licenses/bsl-1.0/';
+  };
+  if (licenseOption === 'The Unlicense') {
+    licenseOption = 'TheUnlicense';
+    licenseLink = 'https://choosealicense.com/licenses/unlicense/';
+  };
+}
+//create variable to hold markdown template and dynamically add to it
+let markdownTemplate =
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
