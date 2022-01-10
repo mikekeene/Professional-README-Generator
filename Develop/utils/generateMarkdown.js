a28c147d9bd65ebf594017de239320df38b98f38
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-01-09 22:08:47 -0600

    generate markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+
 function renderLicenseBadge(license) {}
 function generateMarkdown(data) {
   //license
@@ -57,4 +56,45 @@ function generateMarkdown(data) {
 `;
 }
 
+module.exports = generateMarkdown;
+
+// function to generate markdown for README
+const generateMarkdown = data => {
+  return `# ${data.title}
+  ![Github licence](http://img.shields.io/badge/license-${data.license}-blue.svg)
+
+  
+  ## Description 
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation 
+  ${data.install}
+
+  ## Usage 
+  ${data.usage}
+
+  ## License 
+  This project is license under ${data.license}
+
+  ## Contributing 
+  ${data.contributors}
+
+  ## Tests
+  ${data.test}
+
+  ## Questions
+  If you have any questions about this projects, please contact me directly at ${data.email}. You can view more of my projects at https://github.com/${data.github}.
+`;
+}
+
+
+// use for importing Markdown in index 
 module.exports = generateMarkdown;
