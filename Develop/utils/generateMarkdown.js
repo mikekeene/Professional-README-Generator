6bad7dc32983fc81e0d89c37df70f9848d214e5e
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-01-09 22:09:37 -0600

    generate markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,64 +1,3 @@
-
-function renderLicenseBadge(license) {}
-function generateMarkdown(data) {
-  //license
-  let licenseOption = `${data.license}`;
-  let licenseLink = '';
-  //if conditional to give the license the correct link
-  if (licenseOption === 'GNU AGPLv3') {
-    licenseOption = 'GNUAGPLv3';
-    licenseLink = 'https://choosealicense.com/licenses/agpl-3.0/';
-  };
-  if (licenseOption === 'GNU GPLv3') {
-    licenseOption = 'GNUGPLv3';
-    licenseLink = 'https://choosealicense.com/licenses/gpl-3.0/';
-  };
-  if (licenseOption === 'GNU LGPLv3') {
-    licenseOption = 'GNULGPLv3';
-    licenseLink = 'https://choosealicense.com/licenses/lgpl-3.0/';
-  };
-  if (licenseOption === 'Mozilla Public License 2.0') {
-    licenseOption = 'MozillaPublicLicense2.0';
-    licenseLink = 'https://choosealicense.com/licenses/mpl-2.0/';
-  };
-  if (licenseOption === 'Apache License 2.0') {
-    licenseOption = 'ApacheLicense2.0';
-    licenseLink = 'https://choosealicense.com/licenses/apache-2.0/';
-  };
-  if (licenseOption === 'MIT License') {
-    licenseOption = 'MITLicense';
-    licenseLink = 'https://choosealicense.com/licenses/mit/';
-  };
-  if (licenseOption === 'Boost Software License 1.0') {
-    licenseOption = 'BoostSoftwareLicense1.0';
-    licenseLink = 'https://choosealicense.com/licenses/bsl-1.0/';
-  };
-  if (licenseOption === 'The Unlicense') {
-    licenseOption = 'TheUnlicense';
-    licenseLink = 'https://choosealicense.com/licenses/unlicense/';
-  };
-}
-//create variable to hold markdown template and dynamically add to it
-let markdownTemplate =
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
-
-module.exports = generateMarkdown;
-
-// function to generate markdown for README
 const generateMarkdown = data => {
   return `# ${data.title}
   ![Github licence](http://img.shields.io/badge/license-${data.license}-blue.svg)
@@ -95,6 +34,9 @@ const generateMarkdown = data => {
 `;
 }
 
-
-// use for importing Markdown in index 
 module.exports = generateMarkdown;
+
+
+
+
+
